1decb906b55e2a43a3232022beb7ecb91127241a
wangkaijie <1455276809@qq.com>
2017-04-14 14:20:23 +0800

    remove not useed code

--- tops-fare-galileo/tops-fare-galileo-shopping/src/main/java/com/travelzen/fare/galileo/shopping/route/channel/GalileoShoppingRouteWriteOpType.java
+++
@@ -1,8 +0,0 @@
-package com.travelzen.fare.galileo.shopping.route.channel;
-
-import com.travelzen.fare.util.common.channel.ChannelWriteOpType;
-
-public enum GalileoShoppingRouteWriteOpType implements ChannelWriteOpType{
-	INSERTWITHQUERY, UPDATEVERIFYROUTE;
-
-}
